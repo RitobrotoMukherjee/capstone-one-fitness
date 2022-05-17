5a5b13ef64a942e8d8298e35e5dd080ca9b33286
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-17 20:04:15 +0530

    Add mobile menu desing, and menu toggle functionality in both pages

--- js/main.js
+++ js/main.js
@@ -3,6 +3,23 @@ import { trainers, getHtml } from './featuredObj.js';
 const trainerDiv = document.getElementById('trainer-grid');
 const showCopyrightYear = document.getElementById('current-year');
 
+const toggleMobileMenuBtn = document.querySelectorAll('.toggle-mobile-menu');
+
+const toggleDisplay = (el) => {
+  el.classList.toggle('display-none');
+};
+
+const toggleMobileMenu = () => {
+  const hamburgerBtn = document.getElementById('hamburger-btn');
+  const mobileMenuBg = document.getElementById('mobile-menu-bg');
+  toggleDisplay(hamburgerBtn);
+  toggleDisplay(mobileMenuBg);
+};
+
+toggleMobileMenuBtn.forEach((item) => {
+  item.addEventListener('click', toggleMobileMenu);
+});
+
 trainerDiv.append(getHtml(trainers));
 
 showCopyrightYear.innerText = new Date().getFullYear();
